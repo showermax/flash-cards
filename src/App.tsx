785efc69bb9fad03d59c8f704b872d41415700b7
showermax <sevenhauz@gmail.com>
2023-07-25 14:03:53 +0100

    add polymorphic button

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { Button } from './components'
+
 export function App() {
-  return <div>Hello</div>
+  return (
+    <div>
+      <Button variant="primary" as={'a'} href={'google.com'}>
+        {' '}
+        As link button{' '}
+      </Button>
+      <Button variant="secondary"> Just a button </Button>
+    </div>
+  )
 }
